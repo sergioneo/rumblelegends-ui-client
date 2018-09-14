84518c5b4fa4a6f2438e488e4987a7267b24eda0
Ezequiel Obreque <eandresobrequev@gmail.com>
2018-09-14 01:39:05 -0300

    PATHS SERVICES

--- src/app/modules/legends/constants/constants.prod.ts
+++ src/app/modules/legends/constants/constants.prod.ts
@@ -3,23 +3,23 @@
 import { COMMON } from './constants.common';
 
 const configProd = Object.assign(
-  {},
-  {
-    COMMON,
-    TIMEOUT: 20000,
-    ENDPOINT: 'https://beast-service-beta.herokuapp.com/',
-    SERVICE: {
-      ETHER_CLP_DOLAR: 'https://api.coinmarketcap.com/v2/ticker/1027/?convert=CLP',
-      MARKET_PLACE_BEASTS: 'marketplace/get_beasts',
-      DETAIL_MARKET_PLACE_BEASTS: 'beast',
-    },
-    SERVICES_ERRORS: [
-      {
-        CODE: 1,
-        TYPE: '',
-      },
-    ],
-  }
+	{},
+	{
+		COMMON,
+		TIMEOUT: 20000,
+		ENDPOINT: 'https://beast-service-beta.herokuapp.com/',
+		SERVICE: {
+			ETHER_CLP_DOLAR: 'https://api.coinmarketcap.com/v2/ticker/1027/?convert=CLP',
+			MARKET_PLACE_BEASTS: 'beast/marketplace',
+			DETAIL_MARKET_PLACE_BEASTS: 'beast',
+		},
+		SERVICES_ERRORS: [
+			{
+				CODE: 1,
+				TYPE: '',
+			},
+		],
+	}
 );
 
 export default configProd;
